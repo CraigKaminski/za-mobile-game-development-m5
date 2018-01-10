6713112051d556ec2160a43ddf5c77d0eb29122b
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-01-10 09:11:51 -0500

    Add player bullet prefab. Add player and have them shoot 5 times a second.

---
+++ src/prefabs/PlayerBullet.ts
@@ -0,0 +1,11 @@
+export class PlayerBullet extends Phaser.Sprite {
+  public body: Phaser.Physics.Arcade.Body;
+
+  constructor(game: Phaser.Game, x: number, y: number) {
+    super(game, x, y, 'bullet');
+
+    this.anchor.setTo(0.5);
+    this.checkWorldBounds = true;
+    this.outOfBoundsKill = true;
+  }
+}
--- src/states/Game.ts
+++ src/states/Game.ts
@@ -1,7 +1,12 @@
+import { PlayerBullet } from '../prefabs/PlayerBullet';
+
 export class Game extends Phaser.State {
   private readonly PLAYER_SPEED = 200;
   private readonly BULLET_SPEED = -1000;
   private background: Phaser.TileSprite;
+  private player: Phaser.Sprite;
+  private playerBullets: Phaser.Group;
+  private shootingTimer: Phaser.TimerEvent;
 
   public init() {
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -21,9 +26,43 @@ export class Game extends Phaser.State {
   public create() {
     this.background = this.add.tileSprite(0, 0, this.world.width, this.world.height, 'space');
     this.background.autoScroll(0, 30);
+
+    this.player = this.add.sprite(this.world.centerX, this.world.height - 50, 'player');
+    this.player.anchor.setTo(0.5);
+    this.physics.arcade.enable(this.player);
+    (this.player.body as Phaser.Physics.Arcade.Body).collideWorldBounds = true;
+
+    this.initBullets();
+    this.shootingTimer = this.time.events.loop(Phaser.Timer.SECOND / 5, this.createPlayerBullet, this);
   }
 
   public update() {
+    (this.player.body as Phaser.Physics.Arcade.Body).velocity.x = 0;
 
+    if (this.input.activePointer.isDown) {
+      const targetX = this.input.activePointer.position.x;
+
+      const direction = targetX >= this.world.centerX ? 1 : -1;
+
+      (this.player.body as Phaser.Physics.Arcade.Body).velocity.x = direction * this.PLAYER_SPEED;
+    }
+  }
+
+  private initBullets() {
+    this.playerBullets = this.add.group();
+    this.playerBullets.enableBody = true;
+  }
+
+  private createPlayerBullet() {
+    let bullet: PlayerBullet = this.playerBullets.getFirstExists(false);
+
+    if (!bullet) {
+      bullet = new PlayerBullet(this.game, this.player.x, this.player.top);
+      this.playerBullets.add(bullet);
+    } else {
+      bullet.reset(this.player.x, this.player.top);
+    }
+
+    bullet.body.velocity.y = this.BULLET_SPEED;
   }
 }
